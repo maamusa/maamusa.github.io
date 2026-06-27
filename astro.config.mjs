a0d6bcab07a7588b93013d64ebe4ec720f7789e7
mayankmusaddi <mayankmusaddi1997@gmail.com>
2026-06-27 20:30:25 +0530

    Update astro.config.mjs with site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://maamusa.github.io",
   integrations: [icon()],
   output: "static",
 
