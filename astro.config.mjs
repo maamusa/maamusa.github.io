2b1dd5990c89348be3ba340a02fd1db4e3af531e
mayankmusaddi <mayankmusaddi1997@gmail.com>
2026-06-27 17:46:18 +0530

    Fix projects page and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
 
   markdown: {
     remarkPlugins: [remarkReadingTime],
